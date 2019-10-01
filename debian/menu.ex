65d2dfbd223ba3351486c1a56c037a572bf9927f
dhanvanthari ilangovan <dhanvanthari.ilangovan@arcticwolf.com>
2019-10-01 17:53:07 +0000

    Debianizing libbpf
    libbpf is a library required by suricata to run xdp and ebpf vms.
    This commit debianizes the libbpf library.

---
+++ debian/menu.ex
@@ -0,0 +1,2 @@
+?package(libbpf):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
+  title="libbpf" command="/usr/bin/libbpf"
---
+++ debian/shlibs.local.ex
@@ -0,0 +1,1 @@
+liblibbpf 0.0.5 libbpf (>> 0.0.5-0), libbpf (<< 0.0.5-99)
